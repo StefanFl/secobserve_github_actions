7c71ac3b16080e39e0802858d3152e8d85a2eda8
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-01-21 19:13:42 +0100

    different approach with env

--- SAST/bandit/entrypoint.sh
+++ SAST/bandit/entrypoint.sh
@@ -1,30 +1,15 @@
 #!/bin/sh
 set -e
 
-while getopts "t:r:d:p:" o; do
-   case "${o}" in
-       t)
-         export TARGET=${OPTARG}
-       ;;
-       r)
-         export REPORT_NAME=${OPTARG}
-       ;;
-       d)
-         export RUN_DIRECTORY=${OPTARG}
-       ;;
-       p)
-         export FURTHER_PARAMETERS=${OPTARG}
-       ;;
-  esac
-done
 
-echo $REPORT_NAME
-echo $TARGET
+echo $INPUT_TARGET
+echo $INPUT_REPORT_NAME
 echo $PWD
 echo $HOME
+ls
 
-# cd "$RUN_DIRECTORY"
-bandit $FURTHER_PARAMETERS --format sarif --output "$HOME/$REPORT_NAME" --exit-zero --recursive "$TARGET"
+# cd "$INPUT_RUN_DIRECTORY"
+bandit $INPUT_FURTHER_PARAMETERS --format sarif --output "$INPUT_REPORT_NAME" --exit-zero --recursive "$INPUT_TARGET"
 # cd "$GITHUB_WORKSPACE"
 
 exit 0
