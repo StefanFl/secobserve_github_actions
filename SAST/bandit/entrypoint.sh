49171d6451ff05e000e53d0272501f19d8766a57
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-01-21 19:33:48 +0100

    remove debugging

--- SAST/bandit/entrypoint.sh
+++ SAST/bandit/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-
-echo $INPUT_TARGET
-echo $INPUT_REPORT_NAME
-echo $GITHUB_WORKSPACE
-
 cd "$INPUT_RUN_DIRECTORY"
 bandit $INPUT_FURTHER_PARAMETERS --format sarif --output "$GITHUB_WORKSPACE/$INPUT_REPORT_NAME" --exit-zero --recursive "$INPUT_TARGET"
 cd "$GITHUB_WORKSPACE"
