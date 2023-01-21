8864b4c49b871ae0cadf639938996dde717d1749
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-01-21 19:30:40 +0100

    test with cd

--- SAST/bandit/entrypoint.sh
+++ SAST/bandit/entrypoint.sh
@@ -4,12 +4,10 @@ set -e
 
 echo $INPUT_TARGET
 echo $INPUT_REPORT_NAME
-echo $PWD
-echo $HOME
-ls
+echo $GITHUB_WORKSPACE
 
-# cd "$INPUT_RUN_DIRECTORY"
-bandit $INPUT_FURTHER_PARAMETERS --format sarif --output "$INPUT_REPORT_NAME" --exit-zero --recursive "$INPUT_TARGET"
-# cd "$GITHUB_WORKSPACE"
+cd "$INPUT_RUN_DIRECTORY"
+bandit $INPUT_FURTHER_PARAMETERS --format sarif --output "$GITHUB_WORKSPACE/$INPUT_REPORT_NAME" --exit-zero --recursive "$INPUT_TARGET"
+cd "$GITHUB_WORKSPACE"
 
 exit 0
