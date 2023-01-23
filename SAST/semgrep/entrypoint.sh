73aeba122d0e52546acb08a92b604d8ae51d958a
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-01-23 09:14:05 +0100

    typo

--- SAST/semgrep/entrypoint.sh
+++ SAST/semgrep/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd "$INPUT_RUN_DIRECTORY"
-semgrep scan $INPUT_FURTHER_PARAMETERS --config=$INPUT_CONFIGURATION --metrics=off --no-error --output "$GITHUB_WORKSPACE/$INPUT_REPORT_NAME" --json "$INPUT_TARGET"
+semgrep scan $INPUT_FURTHER_PARAMETERS --config $INPUT_CONFIGURATION --metrics off --no-error --output "$GITHUB_WORKSPACE/$INPUT_REPORT_NAME" --json "$INPUT_TARGET"
 cd "$GITHUB_WORKSPACE"
 
 exit 0
