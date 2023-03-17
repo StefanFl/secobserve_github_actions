9c33d7202ad84249322be8b1e4fe8a6461ea838e
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-03-17 21:00:33 +0100

    debugging

--- docker/entrypoints/entrypoint_checkov.sh
+++ docker/entrypoints/entrypoint_checkov.sh
@@ -11,10 +11,18 @@ else
   WORKSPACE="${GITHUB_WORKSPACE}"
 fi
 
+echo "$RUN_DIRECTORY"
+echo "$TARGET"
+echo "$WORKSPACE"
+echo "$WORKSPACE/$REPORT_NAME"
+
 cd "$RUN_DIRECTORY"
+pwd
 checkov $FURTHER_PARAMETERS --quiet --compact --soft-fail --directory "$TARGET" --output sarif
+ls
 mv results.sarif "$WORKSPACE/$REPORT_NAME"
 cd "$WORKSPACE"
+pwd
 
 if [ "$SO_UPLOAD" == "true" ]; then
   source file_upload_observations.sh
