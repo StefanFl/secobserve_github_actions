8d806756b858701ced0ccded3ee3b17545867ed1
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-03-17 18:03:29 +0100

    move WORKSPACE to entrypoint

--- docker/entrypoints/entrypoint_bandit.sh
+++ docker/entrypoints/entrypoint_bandit.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+if [[ -z "${GITHUB_WORKSPACE}" ]]; then
+  WORKSPACE=.
+else
+  WORKSPACE="${GITHUB_WORKSPACE}"
+fi
+
 cd "$RUN_DIRECTORY"
 bandit $FURTHER_PARAMETERS --format sarif --output "$WORKSPACE/$REPORT_NAME" --exit-zero --recursive "$TARGET"
 cd "$WORKSPACE"
